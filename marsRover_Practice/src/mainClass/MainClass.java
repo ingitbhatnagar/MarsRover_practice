2ea465c37b944896ab0c92af8caf143099928667
IB00431617 <IB00431617@INPUHJPC07310.ATT.TechMahindra.com>
2017-11-15 21:10:08 +0530

    Added sql study notes

--- marsRover_Practice/src/mainClass/MainClass.java
+++ marsRover_Practice/src/mainClass/MainClass.java
@@ -1,10 +1,19 @@
 package mainClass;
+import java.io.*;
 
 public class MainClass {
 
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String[] coordinates=br.readLine().split(" ");
+		int X=Integer.parseInt(coordinates[0]);
+		int Y=Integer.parseInt(coordinates[1]);
+		System.out.println("Enter number of rovers");
+		int number=Integer.parseInt(br.readLine());
+		for(int i=0;i<number;i++)
+		{
+			String[] position=br.readLine().split(" ");
+		}
 	}
 
 }
